e070df37317a33b54945dc21be3456c8834d0d81
Sai2003hub <saiakshaya2003@gmail.com>
2026-01-06 22:44:02 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://crm-backend-0i2h.onrender.com/api',
 });
 
 // Add token to every request
